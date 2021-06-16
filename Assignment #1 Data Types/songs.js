108dc49bbd39af767950d2efa10503ae6c9f24c3
Joseph Amani <77071817+Joe-Amani@users.noreply.github.com>
2021-06-16 09:15:17 +0300

    Add files via upload

---
+++ Assignment #1 Data Types/songs.js
@@ -0,0 +1,60 @@
+//NOTE THAT I AM USING THE BETTER COMMENTS EXTENSION ON VSCODE 
+//I HAVE A PREFERENCE TO COLOR CODING MY COMMENTS
+//! RED
+//? BLUE
+//* GREEN
+//TODO ORANGE
+
+//?OBJECT
+var artist = { 
+     name: "J-Hus",
+     instagram: "theuglygram",
+     instagramFollowers: 889000,
+     city: "London",
+     nationality: "English",
+     career: "Hiphip Rapper",
+     careerStart: 2015,
+     
+};
+//?STRING
+var songName = "Big-Conspiracy";
+var genre = "Hiphop";
+var monthReleased = "January";
+var album = "Big-Conspiracy";
+var salesCertification = "Gold";
+
+//?NUMBERS
+var durationInSeconds = 218;
+var yearReleased = 2020;
+var dateReleased = 24;
+var sales = 100000;
+
+//?BOOLEAN
+sales > 100000;
+//?ARRAY
+var recordLabel = ["Black Butter Records", "Sony Music Uk"];
+var awards = " Brit Award for British Album of the Year";
+var writer = ["Momodou Jallow", "TSB"];
+var producer = "TSB";
+
+
+
+//*LOOGING TO THE CONSOLE
+console.log(songName);
+console.log(artist);
+console.log(genre);
+console.log(durationInSeconds);
+console.log(album);
+console.log(yearReleased);
+console.log(monthReleased);
+console.log(dateReleased);
+console.log(recordLabel);
+console.log(awards);
+console.log(writer);
+console.log(producer);
+console.log(sales);
+console.log(sales<100000);
+console.log(salesCertification);
+
+
+
